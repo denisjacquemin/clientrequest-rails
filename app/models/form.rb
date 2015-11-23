4d6b58313e799f7be8a942c521efd7dcd7fd5389
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-11-23 19:18:01 +0100

    resolving associations between models

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,2 +1,4 @@
 class Form < ActiveRecord::Base
+  belongs_to :type, class_name: 'FormType'
+  belongs_to :author, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  belongs_to :company, dependent: :destroy
+  has_many :forms, dependent: :destroy
+
+
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
