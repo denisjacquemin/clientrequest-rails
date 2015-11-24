c94a63d853f4f5ac526a1e47f4b858347128a0d1
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-11-24 20:18:32 +0100

    adding by_author and by company scopes to Form model

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,4 +2,12 @@ class Form < ActiveRecord::Base
   belongs_to :type, class_name: 'FormType'
   belongs_to :author, class_name: 'User'
   belongs_to :company
+
+  scope :by_author, ->(author_id) { where("author_id = ?", author_id) }
+  scope :by_company, ->(company_id) { where("company_id = ?", company_id) }
+
+  def to_param
+    uid
+  end
+
 end
