8e5fd3203da52c5a01bb972d26222561ea0147a0
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-11-24 20:19:40 +0100

    wip on Forms controller

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,7 +1,9 @@
 class FormsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @form = Form.find_by_uid(params[:uid])
-    render @form.form_type.filename
+    @form = Form.find_by_uid(params[:id])
+    render @form.type.filename
   end
 
   def new
@@ -11,11 +13,11 @@ class FormsController < ApplicationController
   def create
     @form = Form.new
     @form.form_type = params[:type]
-    render @form.form_type.filename
+    render @form.type.filename
   end
 
   def index
-    @forms = Form.all
+    @forms = Form.all.by_company(current_user.company_id)
   end
 
 end
