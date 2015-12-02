0b318b76533c88f7a85f0fcfe8f0a45e161a7116
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-02 10:23:25 +0100

    Forms controller wip

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,23 +1,56 @@
 class FormsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @forms = Form.all.by_company(current_user.company_id)
+    @form = Form.new
+    @types = FormType.all
+  end
+
   def show
     @form = Form.find_by_uid(params[:id])
-    render @form.type.filename
+    render "forms/templates/#{@form.type.filename}"
   end
 
   def new
     @form = Form.new
+    @types = FormType.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
-    @form = Form.new
-    @form.form_type = params[:type]
-    render @form.type.filename
+    @form = Form.new(form_params)
+
+    @form.author_id = current_user.id
+    @form.company_id = current_user.company_id
+    # new_firebase_form_key = FirebaseRef.push('forms', {}) # generate new firebase key (form.uid) under /forms path
+    # @form.uid = new_firebase_form_key.body['name'] # assign key as uid form
+    @form.uid = '98934j3493434jk43s'
+
+    if @form.save
+      render 'create_redirect_to_show.js.erb' # redirect to show screen
+    else
+      @types = FormType.all
+      render :new
+    end
   end
 
-  def index
-    @forms = Form.all.by_company(current_user.company_id)
+  def send_by_email
+    @message = Form.new(params[:message])
+    byebug
+    FormMailer.send_form_email(@message).deliver_later
   end
+
+
+
+  private
+
+     # Never trust parameters from the scary internet, only allow the white list through.
+     def form_params
+       params.require(:form).permit(:type_id, :email, :message)
+     end
 
 end
