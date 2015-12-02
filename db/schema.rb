f3d6255b39894d53dbda13943fbb58e7a9a0ac89
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-02 10:25:15 +0100

    db wip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151124061855) do
+ActiveRecord::Schema.define(version: 20151126174047) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20151124061855) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "company_id"
+    t.string   "email"
+    t.text     "message"
   end
 
   create_table "users", force: :cascade do |t|
