7d59fb383ba363e979f4e56679d94a2d1b918b12
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-03 17:43:13 +0100

    Config "form send message" routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :forms
-  post 'forms/send', to: 'forms#send_by_email', as: 'send_by_email'  
+  post 'forms/send_by_email', to: 'forms#send_by_email', as: 'send_by_email'
+  get 'forms/emails_sent', to: 'forms#emails_sent', as: 'emails_sent'
 
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
