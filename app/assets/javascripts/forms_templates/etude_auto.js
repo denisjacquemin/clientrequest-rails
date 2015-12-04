7cde5a3152db51e4123bbb56e8be63d25619b6ab
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-04 07:03:00 +0100

    adding some behaviours to etude_auto form

--- app/assets/javascripts/forms_templates/etude_auto.js
+++ app/assets/javascripts/forms_templates/etude_auto.js
@@ -1,9 +1,9 @@
-
-$('[name="morethanadriver"]').click(function() {
-  console.log('morethanadrivertrue changed');
-  if ($('#morethanadrivertrue').is(':checked')) {
-    $('#otherdriverdetails').removeClass('hidden');
-  } else {
-    $('#otherdriverdetails').addClass('hidden');
-  }
-})
+$(document).on('ready page:load', function () {
+  $('[name="12"]').click(function() { // Plus d'un conducteur?
+    if ($('#morethanadrivertrue').is(':checked')) {
+      $('#otherdriverdetails').removeClass('hidden');
+    } else {
+      $('#otherdriverdetails').addClass('hidden');
+    }
+  })
+});
