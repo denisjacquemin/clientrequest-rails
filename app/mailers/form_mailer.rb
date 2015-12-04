7f3ad5a33194491eccaf1b2460da93c4838c625c
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-04 06:45:44 +0100

    improving mail sent

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,6 +1,11 @@
 class FormMailer < ApplicationMailer
   def send_form_email(message)
     @message = message
-    mail(to: @message.email, subject: 'Welcome to My Awesome Site')
+    
+
+    mail(to: @message.email,
+         reply_to: @message.author.email,
+         from: @message.author.name,
+         subject: "#{@message.form.type.label}")
   end
 end
