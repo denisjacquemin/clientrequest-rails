ad1adf8abe2fd1abd1a38cb58c49939724a14a57
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-06 16:45:33 +0100

    'from' is an email address

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,11 +1,9 @@
 class FormMailer < ApplicationMailer
   def send_form_email(message)
     @message = message
-    
-
     mail(to: @message.email,
          reply_to: @message.author.email,
-         from: @message.author.name,
+         from: @message.author.email,
          subject: "#{@message.form.type.label}")
   end
 end
