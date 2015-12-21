6ebffe1290b4eb755a74949a106dbf1f621b2cbd
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-21 15:27:02 +0100

    removing message migrations

--- db/migrate/20151126174047_add_email_and_message_to_forms.rb
+++
@@ -1,7 +0,0 @@
-class AddEmailAndMessageToForms < ActiveRecord::Migration
-  def change
-    add_column :forms, :to, :string
-    add_column :forms, :from, :string
-    add_column :forms, :message, :text
-  end
-end
--- db/migrate/20151202135642_add_columns_to_message.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToMessage < ActiveRecord::Migration
-  def change
-    add_column :messages, :author_id, :integer
-    add_column :messages, :form_id, :integer
-    add_column :messages, :form_as_pdf, :boolean
-  end
-end
