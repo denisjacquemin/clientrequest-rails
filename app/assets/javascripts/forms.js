6ca65920427ace2700da81acd22a8d95197af055
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-21 15:34:57 +0100

    improving autosave for forms

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -7,34 +7,43 @@ $(document).on('ready page:load', function () {
 
   var form_uid = $('#form_uid').val();
   if (form_uid != undefined) {
-    firebaseRef.child('forms').child(form_uid).once('value', function(snap) {
+    firebaseRef.child('forms').child(form_uid).on('value', function(snap) {
       snap.forEach(function(childSnapshot) {
         var selector = '#' + childSnapshot.key();
         var elem = $(selector)
 
         if (elem.is( "[type=text]" )) {
           elem.val(childSnapshot.val().value);
-        } else if (elem.is(':radio')) {
-          $('input[name="' + childSnapshot.key() + '"]').each(function(r) {
+        } else if (elem.is(':checkbox')) {
+            elem.prop("checked", true);
+        } else if ($('input[name="' + childSnapshot.key() + '"]').is(':radio')) {
+          $('input[name="' + childSnapshot.key() + '"]').each(function(i, r) {
             if (r.value === childSnapshot.val().value) {
-              r.prop("checked", true);
+              $(r).prop("checked", true);
             } else {
-              r.prop("checked", false);
+              $(r).prop("checked", false);
             }
           });
-        }  // end else if
+        }
       });
     })
   }
 
 
   // observe changes and save them to Firebase
-  $('.fb input[type="text"]').keyup(function(event) {
+  $('.fb input[type="text"], .fb textarea').keyup(function(event) {
     firebaseRef.child('forms').child(form_uid).child(event.target.id).update({"value": event.target.value});
   });
 
   $('.fb input:radio').change(function(event) {
-    console.log('saving ' + event.target.value + ' to forms/' + form_uid + '/' + event.target.name);
     firebaseRef.child('forms').child(form_uid).child(event.target.name).update({"value": event.target.value});
+  });
+
+  $('.fb input:checkbox').change(function(event) {
+    if (event.target.checked) {
+      firebaseRef.child('forms').child(form_uid).child(event.target.id).update({"value": event.target.value});
+    } else {
+      firebaseRef.child('forms').child(form_uid).child(event.target.id).remove();
+    }
   });
 });
