134d9ca3258dedc695b9967f31d0cab39eb1b24d
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-23 03:48:54 +0100

    adding show_for_pdf route and invitation controller customized

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   resources :forms
   post 'forms/create_send_form_message', to: 'forms#create_send_form_message'
   get 'forms/emails_sent_by_form', to: 'forms#emails_sent_by_form'
+  get 'forms/show_for_pdf/:id', to: 'forms#show_for_pdf', as: 'show_for_pdf'
+
 
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, :controllers => { invitations: 'users/invitations' }
   resources :users
 end
