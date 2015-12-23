8a27573579f621b59e530b015c84de6f200b8514
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-23 03:29:45 +0100

    attach or not the form as pdf

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -1,9 +1,15 @@
 class FormMailer < ApplicationMailer
   def send_form_email(message)
     @message = message
+
+    if @message.form_as_pdf # show_for_pdf_url(@message.form.uid, as_pdf: 'true')
+      pdf = WickedPdf.new.pdf_from_url(show_for_pdf_url(@message.form.uid))
+      attachments["#{@message.form.type.label}.pdf"] = pdf
+    end
+
     mail(to: @message.to,
-         reply_to: @message.from,
-         from: @message.from,
+         reply_to: @message.author.email,
+         from: @message.author.email,
          subject: "#{@message.form.type.label}")
   end
 end
