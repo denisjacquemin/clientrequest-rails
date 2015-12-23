8dbd5f4ae7cd6b28877bfe1fb04c090d1afe869c
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-23 03:49:16 +0100

    message migration updated

--- db/migrate/20151202094738_create_messages.rb
+++ db/migrate/20151202094738_create_messages.rb
@@ -1,8 +1,11 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :email
       t.text :content
+      t.string :to
+      t.integer :author_id
+      t.integer :form_id
+      t.boolean :form_as_pdf
 
       t.timestamps null: false
     end
