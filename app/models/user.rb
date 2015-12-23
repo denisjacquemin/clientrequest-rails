4bbea8ae66c9c852f76e34614e3135b7574997b1
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-23 03:29:58 +0100

    adding by_company scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  scope :by_company, ->(company_id) { where("company_id = ?", company_id) }
+
   def set_default_role
     self.role ||= :user
   end
