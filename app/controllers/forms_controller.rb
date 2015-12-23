df12dab73c1dcbbdce340e306d4238e2546e647f
Denis Jacquemin <denis.jacquemin@gmail.com>
2015-12-23 03:21:29 +0100

    generate "for pdf" route added

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,5 +1,5 @@
 class FormsController < ApplicationController
-  before_action :authenticate_user!, except: [:show, :create_send_form_message]
+  before_action :authenticate_user!, except: [:show, :create_send_form_message, :show_for_pdf]
 
   def index
     @forms = Form.all.by_company(current_user.company_id)
@@ -14,15 +14,28 @@ class FormsController < ApplicationController
 
     respond_to do |format|
       format.html do
-        render "forms/templates/#{@form.type.filename}"
+        if (params[:as_pdf])
+          render "forms/templates/#{@form.type.filename}_pdf",  :layout => "application_pdf.html.erb"
+        else
+          render "forms/templates/#{@form.type.filename}"
+        end
       end
       format.pdf do
+        # data = WickedPdf.new.pdf_from_url(show_for_pdf_url(@form.uid))
+        # send_data( data, :filename => "#{@form.type.label}.pdf" )
         render :pdf => "file_name",
                :template => "forms/templates/#{@form.type.filename}",
                :layout => "application_pdf.html.erb",
                :javascript_delay => 2000
       end
     end
+  end
+
+  def show_for_pdf
+    @form = Form.find_by_uid(params[:id])
+    res = FirebaseRef.get "/forms/#{@form.uid}"
+    @data = res.body.to_json
+    render "forms/templates/#{@form.type.filename}_pdf",  :layout => "application_pdf.html.erb"
   end
 
   def new
@@ -39,9 +52,9 @@ class FormsController < ApplicationController
 
     @form.author_id = current_user.id
     @form.company_id = current_user.company_id
-    # new_firebase_form_key = FirebaseRef.push('forms', {}) # generate new firebase key (form.uid) under /forms path
-    # @form.uid = new_firebase_form_key.body['name'] # assign key as uid form
-    @form.uid = '98934j3493434jk43s' + Date.new().to_s
+    new_firebase_form_key = FirebaseRef.push('forms', {}) # generate new firebase key (form.uid) under /forms path
+    @form.uid = new_firebase_form_key.body['name'] # assign key as uid form
+    #@form.uid = '98934j3493434jk43s' + Date.new().to_s
 
     if @form.save
       redirect_to form_url(@form.uid) # redirect to show screen
@@ -53,6 +66,7 @@ class FormsController < ApplicationController
 
   def create_send_form_message
     @message = Message.new(message_params)
+    @message.author_id = current_user.id
     @message.save
     @messages = Message.by_form(@message.form_id)
 
@@ -72,7 +86,7 @@ class FormsController < ApplicationController
      end
 
      def message_params
-       params.require(:message).permit(:to, :content, :from, :form_id, :form_as_pdf)
+       params.require(:message).permit(:to, :content, :form_id, :form_as_pdf)
      end
 
 end
