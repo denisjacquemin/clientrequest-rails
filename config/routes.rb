cbf556b9657c8c04c22e72c2e92d202ee61d3741
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-01-05 06:52:39 +0100

    adding edit and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'companies/edit', to: 'companies#edit', as: 'edit_company'
+  patch 'companies/update', to: 'companies#update', as: 'update_company'
+
   resources :forms
   post 'forms/create_send_form_message', to: 'forms#create_send_form_message'
   get 'forms/emails_sent_by_form', to: 'forms#emails_sent_by_form'
